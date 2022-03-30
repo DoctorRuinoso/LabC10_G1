b5f549f79ac8fa85f52221dd57708590900840c1
DoctorRuinoso <alejandro.guerreromedina@alum.uca.es>
2022-03-30 17:28:06 +0200

    Administrador_usuariosmaterias (V2)
    He añadido comentarios y tengo que probar el módulo en sí

--- lib/administrador_usuariosmaterias.c
+++ lib/administrador_usuariosmaterias.c
@@ -1,21 +1,28 @@
 //FUNCIONES USUARIOS Y MATERIAS DEL MODULO ADMINISTRADOR
 
-#include "administrador_usuariosmaterias.h"
+#include "lib/core.h"
+#include "lib/administrador_usuariosmaterias.h"
 
+//Cabecera: void menu_administrador()
+//Precondición: el usuario ha seleccionado el perfil administrador.
+//Postcondición: se muestran las opciones del menu administrador: usuarios, alumnos, materias, horarios.
 void menu_administrador(){
     int opc;
     do{
         printf(" ____________________________________________________________\n |Menu:Administrador                                        |\n |----------------------------------------------------------|\n |                  1.Usuario                               |\n |                  2.Alumnos                               |\n |                  3.Materias                              |\n |                  4.Horarios                              |\n |__________________________________________________________|\n");
         scanf("%i",&opc);
-    }while(opc<1 || opc>4);ç
+    }while(opc<1 || opc>4);//fin_do_While
     switch(opc){
-        case 1: opcion_usuarios();break;
-        case 2: opcion_alumnos();break;
-        case 3: opcion_materias();break;
-        case 4: opcion_horarios();break;
-    }
-}
+        case 1: system("cls"); opcion_usuarios();break;
+        case 2: system("cls"); opcion_alumnos();break;
+        case 3: system("cls"); opcion_materias();break;
+        case 4: system("cls"); opcion_horarios();break;
+    }//fin_switch
+}//fin_void
 
+//Cabecera: void opcion_usuarios()
+//Precondición: el administrador ha seleccionado la opcion 1: usuarios.
+//Postcondición: permite acceder a las distintas funcionalidades del menu usuario: dar de alta, dar de baja, modificar y listar usuarios.
 void opcion_usuarios(){
 	int opc, salida = 0;
 	do{
@@ -27,10 +34,13 @@ void opcion_usuarios(){
             	case 3: modificar_usuario(); break;
             	case 4: listar_usuarios(); break;
             	case 5: salida=1; break;
-        	}
-    }while(opc<1 || opc>5 && salida==0);
-}
+        	}//fin_switch
+    }while(opc<1 || opc>5 && salida==0);//fin_do_while
+}//fin_void
 
+//Cabecera: void opcion_materias()
+//Precondición: el administrador ha seleccionado la opcion 3: materias.
+//Postcondición: permite acceder a las distintas funcionalidades del menú materias: dar de alta, dar de baja, modificar y listar materias.
 void opcion_materias(){
 	int opc, salida = 0;
 	do{
@@ -42,40 +52,15 @@ void opcion_materias(){
             	case 3: modificar_materia(); break;
             	case 4: listar_materias(); break;
             	case 5: salida=1; break;
-        	}
-    }while(opc<1 || opc>5 && salida==0);
-}
+        	}//fin_switch
+    }while(opc<1 || opc>5 && salida==0);//fin_do_while
+}//fin_void
 
 //Mini módulo Administrador - Usuarios
 
-void alta_usuario(){
-	USUARIOS_FILE = fopen("a","data/usuarios.txt");
-	assert(USUARIOS_FILE!=NULL);
-
-	usuario alta_usuario;
-	printf("Introduce el id\n");
-	scanf("%s", alta_usuario.id);
-	printf("Introduce el nombre completo del usuario\n");
-	scanf("%s", alta_usuario.nombre);
-	printf("Introduce el perfil del usuario: administrador o profesor\n");
-	scanf("%s", alta_usuario.perfil);
-	printf("Introduce el nombre de usuario\n");
-	scanf("%s", alta_usuario.name_tag);
-	printf("Introduce la password\n");
-	scanf("%s", alta_usuario.password);
-	
-	fprintf(ALUMNOS, "%s", alta_usuario.id);
-	fprintf(ALUMNOS, "%c", "\n");
-	fprintf(ALUMNOS, "%s", alta_usuario.nombre);
-	fprintf(ALUMNOS, "%c", "\n");
-	fprintf(ALUMNOS, "%s", alta_usuario.perfil);
-	fprintf(ALUMNOS, "%c", "\n");
-	fprintf(ALUMNOS, "%s", alta_usuario.name_tag);
-	fprintf(ALUMNOS, "%c", "\n");
-	fprintf(ALUMNOS, "%s", alta_usuario.password);
-	fclose(ALUMNOS);
-}
-
+//Cabecera: void baja_usuario()
+//Precondición: ninguna.
+//Postcondición: elimina al usuario cuyo id ha sido seleccionado satisfactoriamente.
 void baja_usuario(){
     char idequal[3];
     int cont = 0;
@@ -91,12 +76,16 @@ void baja_usuario(){
 		  strcpy(usuario[i].perfil, "");
 		  strcpy(usuario[i].name_tag, "");
 		  strcpy(usuario[i].password, "");
-        }
-    }
+        }//fin_if
+    }//fin_for
+
     if(counter == 0) printf("\nEl identificador introducido es incorrecto o no existe\n");
     core_usuarios_update;
-}
+}//fin_void
 
+//Cabecera: void modificar_usuario()
+//Precondición: ninguna.
+//Postcondición: modifica el campo seleccionado por el administrador para el usuario cuya id sea correspondiente satisfactoriamente.
 void modificar_usuario(){
 	printf("Introduce el ID del usuario a modificar: \n");
 	scanf("%s", usuario.id);
@@ -127,25 +116,31 @@ void modificar_usuario(){
 					printf("Introduce la password\n");
 					scanf("%s", usuario[i].password);
 					break;
-			}
+			}//fin_switch
 			core_usuarios_update();
-		}
-	}
-}
+		}//fin_if
+	}//fin_for
+}//fin_void
 
+//Cabecera: void listar_usuarios()
+//Precondición: ninguna.
+//Postcondición: muestra por pantalla la lista de usuarios disponibles.
 void listar_usuarios(){
 	assert(USUARIOS_FILE!=NULL);
 	for (int i = 0; i < configuration.usuarios_counter; ++i){
         printf("ID: %s\n", usuario[i].id);
         printf("Nombre Completo del Usuario: %s\n", usuario[i].nombre);
         printf("Perfil: %s\n", usuario[i].perfil);
-	  printf("Name Tag: %s\n", usuario[i].name_tag);
-	  printf("Password: %s\n", usuario[i].password);
-	}
-}
+	   printf("Name Tag: %s\n", usuario[i].name_tag);
+	   printf("Password: %s\n", usuario[i].password);
+	}//fin_for
+}//fin_void
 
 //Mini módulo Administrador - Materias
 
+//Cabecera: void alta_materia()
+//Precondición: fichero "materias.txt" existente.
+//Postcondición: guarda en el fichero "materias.txt" una nueva materia satisfactoriamente.
 void alta_materia(){
 	MATERIAS_FILE = fopen("a","data/materias.txt");
 	assert(MATERIAS_FILE!=NULL);
@@ -165,8 +160,12 @@ void alta_materia(){
 	fprintf(MATERIAS, "%s", alta_materia.abrev);
 	fprintf(MATERIAS, "%c", "\n");
 	fclose(MATERIAS);
-}
+	core_materias_update();
+}//fin_void
 
+//Cabecera: void baja_materia()
+//Precondición: ninguna.
+//Postcondición: elimina la materia cuyo id ha sido seleccionado satisfactoriamente.
 void baja_materia(){
     char idequal[4];
     int cont = 0;
@@ -186,6 +185,9 @@ void baja_materia(){
     core_materias_update;
 }
 
+//Cabecera: void modificar_materia()
+//Precondición: ninguna.
+//Postcondición: modifica el campo seleccionado por el administrador para la materia cuya id sea correspondiente satisfactoriamente.
 void modificar_materia(){
 	printf("Introduce el ID de la materia a modificar: \n");
 	scanf("%s", materia.id);
@@ -214,6 +216,9 @@ void modificar_materia(){
 	}
 }
 
+//Cabecera: void listar_materias()
+//Precondición: ninguna.
+//Postcondición: muestra por pantalla la lista de materias disponibles.
 void listar_materias(){
 	assert(MATERIAS_FILE!=NULL);
 	for (int i = 0; i < configuration.materias_counter; ++i){
